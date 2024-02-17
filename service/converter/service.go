95abb261aa9861bc2d5f47fa540a0a9832958dd4
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2024-02-17 22:13:47 +0200

    save [skio ci]

--- service/converter/service.go
+++ service/converter/service.go
@@ -2,11 +2,13 @@ package converter
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/cloudevents/sdk-go/binding/format/protobuf/v2/pb"
 	vocab "github.com/go-ap/activitypub"
 	"github.com/google/uuid"
 	"google.golang.org/protobuf/types/known/timestamppb"
+	"reflect"
 	"strings"
 )
 
@@ -23,45 +25,48 @@ const CeKeyAction = "action"
 const CeKeyAttachment = "attachment"
 const CeKeyAttachmentType = "attachmenttype"
 const CeKeyAudience = "audience"
-const CeKeyAuthor = "author"
 const CeKeyCategories = "categories"
 const CeKeyCc = "cc"
+const CeKeyDuration = "duration"
+const CeKeyEnds = "ends"
+const CeKeyIcon = "icon"
 const CeKeyImage = "image"
 const CeKeyInReplyTo = "inreplyto"
 const CeKeyLatitude = "latitude"
 const CeKeyLongitude = "longitude"
+const CeKeyObject = "object"
+const CeKeyObjectUrl = "objecturl"
 const CeKeyPreview = "preview"
+const CeKeyStarts = "starts"
 const CeKeySubject = "subject"
 const CeKeySummary = "summary"
 const CeKeyTime = "time"
+const CeKeyUpdated = "updated"
 
-const fmtAuthor = "<a href=\"%s\">%s</a>"
+var ErrFail = errors.New("failed to convert")
 
 func NewService() Service {
 	return service{}
 }
 
 func (svc service) Convert(ctx context.Context, actor vocab.Actor, activity vocab.Activity) (evt *pb.CloudEvent, err error) {
-	// mastodon produces too much self delete activities, skip these
 	t := activity.Type
-	if t == vocab.DeleteType && activity.Actor == activity.Object {
-		return
+	if t == "" || t == vocab.DeleteType && activity.Actor == activity.Object {
+		return // skip self delete activities w/o an error
 	}
-	//
-	evt, err = svc.convertActivity(ctx, actor, activity)
-	//
+	evt, err = svc.convertActivity(actor, activity)
 	return
 }
 
-func (svc service) convertActivity(ctx context.Context, actor vocab.Actor, activity vocab.Activity) (evt *pb.CloudEvent, err error) {
+func (svc service) convertActivity(actor vocab.Actor, activity vocab.Activity) (evt *pb.CloudEvent, err error) {
 	//
 	evt = &pb.CloudEvent{
 		Id:          uuid.NewString(),
-		Source:      actor.GetID().String(),
+		Source:      actor.ID.String(),
 		SpecVersion: CeSpecVersion,
 		Type:        CeType,
 		Attributes: map[string]*pb.CloudEventAttributeValue{
-			CeKeyAuthor: {
+			CeKeySubject: {
 				Attr: &pb.CloudEventAttributeValue_CeString{
 					CeString: actor.Name.String(),
 				},
@@ -73,35 +78,277 @@ func (svc service) convertActivity(ctx context.Context, actor vocab.Actor, activ
 			},
 		},
 	}
+	if activity.Content != nil {
+		evt.Data = &pb.CloudEvent_TextData{
+			TextData: activity.Content.String(),
+		}
+	}
+	if activity.Summary != nil {
+		txt := evt.GetTextData()
+		switch txt {
+		case "":
+			evt.Data = &pb.CloudEvent_TextData{
+				TextData: activity.Summary.String(),
+			}
+		default:
+			evt.Data = &pb.CloudEvent_TextData{
+				TextData: fmt.Sprintf("%s\n\n%s", activity.Summary, txt),
+			}
+		}
+	}
 	//
 	t := string(activity.Type)
 	switch {
-	case activity.Object != nil && activity.Object.IsObject():
+	case activity.Object != nil:
 		evt.Attributes[CeKeyAction] = &pb.CloudEventAttributeValue{
 			Attr: &pb.CloudEventAttributeValue_CeString{
 				CeString: t,
 			},
 		}
-		err = svc.convertObject(ctx, activity.Object.(*vocab.Object), evt)
-	default:
-		evt.Attributes[CeKeySubject] = &pb.CloudEventAttributeValue{
-			Attr: &pb.CloudEventAttributeValue_CeString{
-				CeString: t,
-			},
+		obj := activity.Object
+		switch {
+		case obj.IsLink():
+			evt.Attributes[CeKeyObjectUrl] = &pb.CloudEventAttributeValue{
+				Attr: &pb.CloudEventAttributeValue_CeUri{
+					CeUri: obj.GetLink().String(),
+				},
+			}
+		case obj.IsObject():
+			err = svc.convertObject(obj.(*vocab.Object), evt)
+		default:
+			err = fmt.Errorf("%w activity object, unexpected type: %s", ErrFail, reflect.TypeOf(obj))
 		}
-		// TODO
+	default:
+		err = svc.convertActivityAsObject(activity, evt)
 	}
 	//
 	return
 }
 
-func (svc service) convertObject(ctx context.Context, obj *vocab.Object, evt *pb.CloudEvent) (err error) {
-	evt.Attributes[CeKeySubject] = &pb.CloudEventAttributeValue{
+func (svc service) convertObject(obj *vocab.Object, evt *pb.CloudEvent) (err error) {
+	evt.Attributes[CeKeyObject] = &pb.CloudEventAttributeValue{
 		Attr: &pb.CloudEventAttributeValue_CeString{
 			CeString: string(obj.Type),
 		},
 	}
-	if att := obj.Attachment; att != nil && att.IsObject() {
+	evt.Attributes[CeKeyObjectUrl] = &pb.CloudEventAttributeValue{
+		Attr: &pb.CloudEventAttributeValue_CeUri{
+			CeUri: obj.ID.String(),
+		},
+	}
+	if att := obj.Attachment; att != nil {
+		err = convertAttachment(att, evt)
+	}
+	if aud := obj.Audience; aud != nil && len(aud) > 0 {
+		err = errors.Join(err, convertAsCollection(aud, evt, CeKeyAudience))
+	}
+	if cc := obj.CC; cc != nil && len(cc) > 0 {
+		err = errors.Join(err, convertAsCollection(cc, evt, CeKeyCc))
+	}
+	if obj.Duration > 0 {
+		evt.Attributes[CeKeyDuration] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeInteger{
+				CeInteger: int32(obj.Duration.Seconds()),
+			},
+		}
+	}
+	if !obj.EndTime.IsZero() {
+		evt.Attributes[CeKeyEnds] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeTimestamp{
+				CeTimestamp: timestamppb.New(obj.EndTime),
+			},
+		}
+	}
+	if ico := obj.Icon; ico != nil {
+		err = errors.Join(err, convertAsLink(ico, evt, CeKeyIcon))
+	}
+	if img := obj.Image; img != nil {
+		err = errors.Join(err, convertAsLink(img, evt, CeKeyImage))
+	}
+	if inReplyTo := obj.InReplyTo; inReplyTo != nil {
+		err = errors.Join(err, convertInReplyTo(inReplyTo, evt))
+	}
+	if loc := obj.Location; loc != nil {
+		err = errors.Join(err, convertLocation(loc, evt))
+	}
+	if preview := obj.Preview; preview != nil {
+		err = errors.Join(err, convertAsLink(preview, evt, CeKeyPreview))
+	}
+	if !obj.StartTime.IsZero() {
+		evt.Attributes[CeKeyStarts] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeTimestamp{
+				CeTimestamp: timestamppb.New(obj.StartTime),
+			},
+		}
+	}
+	if summ := obj.Summary; summ != nil && len(summ) > 0 {
+		txt := evt.GetTextData()
+		switch txt {
+		case "":
+			evt.Data = &pb.CloudEvent_TextData{
+				TextData: summ.String(),
+			}
+		default:
+			evt.Data = &pb.CloudEvent_TextData{
+				TextData: fmt.Sprintf("%s\n\n%s", summ, txt),
+			}
+		}
+	}
+	if tags := obj.Tag; tags != nil && len(tags) > 0 {
+		err = errors.Join(err, convertAsCollection(tags, evt, CeKeyCategories))
+	}
+	if !obj.Updated.IsZero() {
+		evt.Attributes[CeKeyUpdated] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeTimestamp{
+				CeTimestamp: timestamppb.New(obj.Updated),
+			},
+		}
+	}
+	if obj.Content != nil {
+		txt := evt.GetTextData()
+		switch txt {
+		case "":
+			evt.Data = &pb.CloudEvent_TextData{
+				TextData: obj.Content.String(),
+			}
+		default:
+			evt.Data = &pb.CloudEvent_TextData{
+				TextData: fmt.Sprintf("%s\n\n%s", obj.Content, txt),
+			}
+		}
+	}
+	return
+}
+
+func (svc service) convertActivityAsObject(obj vocab.Activity, evt *pb.CloudEvent) (err error) {
+	evt.Attributes[CeKeyObject] = &pb.CloudEventAttributeValue{
+		Attr: &pb.CloudEventAttributeValue_CeString{
+			CeString: string(obj.Type),
+		},
+	}
+	evt.Attributes[CeKeyObjectUrl] = &pb.CloudEventAttributeValue{
+		Attr: &pb.CloudEventAttributeValue_CeUri{
+			CeUri: obj.ID.String(),
+		},
+	}
+	if att := obj.Attachment; att != nil {
+		err = convertAttachment(att, evt)
+	}
+	if aud := obj.Audience; aud != nil && len(aud) > 0 {
+		err = errors.Join(err, convertAsCollection(aud, evt, CeKeyAudience))
+	}
+	if cc := obj.CC; cc != nil && len(cc) > 0 {
+		err = errors.Join(err, convertAsCollection(cc, evt, CeKeyCc))
+	}
+	if obj.Duration > 0 {
+		evt.Attributes[CeKeyDuration] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeInteger{
+				CeInteger: int32(obj.Duration.Seconds()),
+			},
+		}
+	}
+	if !obj.EndTime.IsZero() {
+		evt.Attributes[CeKeyEnds] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeTimestamp{
+				CeTimestamp: timestamppb.New(obj.EndTime),
+			},
+		}
+	}
+	if ico := obj.Icon; ico != nil {
+		err = errors.Join(err, convertAsLink(ico, evt, CeKeyIcon))
+	}
+	if img := obj.Image; img != nil {
+		err = errors.Join(err, convertAsLink(img, evt, CeKeyImage))
+	}
+	if inReplyTo := obj.InReplyTo; inReplyTo != nil {
+		err = errors.Join(err, convertInReplyTo(inReplyTo, evt))
+	}
+	if loc := obj.Location; loc != nil {
+		err = errors.Join(err, convertLocation(loc, evt))
+	}
+	if preview := obj.Preview; preview != nil {
+		err = errors.Join(err, convertAsLink(preview, evt, CeKeyPreview))
+	}
+	if !obj.StartTime.IsZero() {
+		evt.Attributes[CeKeyStarts] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeTimestamp{
+				CeTimestamp: timestamppb.New(obj.StartTime),
+			},
+		}
+	}
+	if summ := obj.Summary; summ != nil && len(summ) > 0 {
+		txt := evt.GetTextData()
+		switch txt {
+		case "":
+			evt.Data = &pb.CloudEvent_TextData{
+				TextData: summ.String(),
+			}
+		default:
+			evt.Data = &pb.CloudEvent_TextData{
+				TextData: fmt.Sprintf("%s\n\n%s", summ, txt),
+			}
+		}
+		err = errors.Join(err, convertAsText(summ, evt, CeKeySummary))
+	}
+	if tags := obj.Tag; tags != nil && len(tags) > 0 {
+		err = errors.Join(err, convertAsCollection(tags, evt, CeKeyCategories))
+	}
+	if !obj.Updated.IsZero() {
+		evt.Attributes[CeKeyUpdated] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeTimestamp{
+				CeTimestamp: timestamppb.New(obj.Updated),
+			},
+		}
+	}
+	if obj.Content != nil {
+		txt := evt.GetTextData()
+		switch txt {
+		case "":
+			evt.Data = &pb.CloudEvent_TextData{
+				TextData: obj.Content.String(),
+			}
+		default:
+			evt.Data = &pb.CloudEvent_TextData{
+				TextData: fmt.Sprintf("%s\n\n%s", obj.Content, txt),
+			}
+		}
+	}
+	return
+}
+
+func convertAsCollection(cc vocab.ItemCollection, evt *pb.CloudEvent, key string) (err error) {
+	var ccs []string
+	for _, ccItem := range cc {
+		var ccStr string
+		switch {
+		case ccItem.IsLink():
+			ccStr = ccItem.GetLink().String()
+		case ccItem.IsObject():
+			ccStr = ccItem.(*vocab.Object).Name.String()
+		default:
+			err = fmt.Errorf("%w item in the collection, unexpected type: %s", ErrFail, reflect.TypeOf(ccItem))
+		}
+		if ccStr != "" {
+			ccs = append(ccs, ccStr)
+		}
+	}
+	evt.Attributes[key] = &pb.CloudEventAttributeValue{
+		Attr: &pb.CloudEventAttributeValue_CeString{
+			CeString: strings.Join(ccs, " "),
+		},
+	}
+	return
+}
+
+func convertAttachment(att vocab.Item, evt *pb.CloudEvent) (err error) {
+	switch {
+	case att.IsLink():
+		evt.Attributes[CeKeyAttachment] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeUri{
+				CeUri: att.GetLink().String(),
+			},
+		}
+	case att.IsObject():
 		attObj := att.(*vocab.Object)
 		evt.Attributes[CeKeyAttachment] = &pb.CloudEventAttributeValue{
 			Attr: &pb.CloudEventAttributeValue_CeUri{
@@ -113,117 +360,65 @@ func (svc service) convertObject(ctx context.Context, obj *vocab.Object, evt *pb
 				CeString: string(attObj.MediaType),
 			},
 		}
+	default:
+		err = fmt.Errorf("%w attachment, unexpected type: %s", ErrFail, reflect.TypeOf(att))
 	}
-	if aud := obj.Audience; aud != nil && len(aud) > 0 {
-		var auds []string
-		for _, audItem := range aud {
-			if audItem.IsObject() {
-				audName := audItem.(*vocab.Object).Name.String()
-				auds = append(auds, audName)
-			}
-		}
-		evt.Attributes[CeKeyAudience] = &pb.CloudEventAttributeValue{
-			Attr: &pb.CloudEventAttributeValue_CeString{
-				CeString: strings.Join(auds, " "),
-			},
-		}
+	return
+}
+
+func convertAsLink(item vocab.Item, evt *pb.CloudEvent, key string) (err error) {
+	evt.Attributes[key] = &pb.CloudEventAttributeValue{
+		Attr: &pb.CloudEventAttributeValue_CeUri{
+			CeUri: item.GetLink().String(),
+		},
 	}
-	if cc := obj.CC; cc != nil && len(cc) > 0 {
-		var ccs []string
-		for _, ccItem := range cc {
-			var ccStr string
-			switch {
-			case ccItem.IsLink():
-				ccStr = ccItem.GetLink().String()
-			case ccItem.IsObject():
-				ccStr = ccItem.(*vocab.Object).Name.String()
-			default:
-				// TODO err
-			}
-			if ccStr != "" {
-				ccs = append(ccs, ccStr)
-			}
-		}
-		evt.Attributes[CeKeyCc] = &pb.CloudEventAttributeValue{
-			Attr: &pb.CloudEventAttributeValue_CeString{
-				CeString: strings.Join(ccs, " "),
-			},
-		}
+	return
+}
+
+func convertAsText(item vocab.NaturalLanguageValues, evt *pb.CloudEvent, key string) (err error) {
+	evt.Attributes[key] = &pb.CloudEventAttributeValue{
+		Attr: &pb.CloudEventAttributeValue_CeUri{
+			CeUri: item.String(),
+		},
 	}
-	if img := obj.Image; img != nil {
-		evt.Attributes[CeKeyImage] = &pb.CloudEventAttributeValue{
+	return
+}
+
+func convertInReplyTo(inReplyTo vocab.Item, evt *pb.CloudEvent) (err error) {
+	switch {
+	case inReplyTo.IsLink():
+		evt.Attributes[CeKeyInReplyTo] = &pb.CloudEventAttributeValue{
 			Attr: &pb.CloudEventAttributeValue_CeUri{
-				CeUri: img.GetLink().String(),
+				CeUri: inReplyTo.GetLink().String(),
 			},
 		}
-	}
-	if inReplyTo := obj.InReplyTo; inReplyTo != nil {
-		switch {
-		case inReplyTo.IsLink():
-			evt.Attributes[CeKeyImage] = &pb.CloudEventAttributeValue{
-				Attr: &pb.CloudEventAttributeValue_CeUri{
-					CeUri: inReplyTo.GetLink().String(),
-				},
-			}
-		case inReplyTo.IsObject():
-			evt.Attributes[CeKeyImage] = &pb.CloudEventAttributeValue{
-				Attr: &pb.CloudEventAttributeValue_CeString{
-					CeString: inReplyTo.(*vocab.Object).Name.String(),
-				},
-			}
-		default:
-			// TODO err
-		}
-	}
-	if loc := obj.Location; loc != nil {
-		switch locT := loc.(type) {
-		case *vocab.Place:
-			evt.Attributes[CeKeyLatitude] = &pb.CloudEventAttributeValue{
-				Attr: &pb.CloudEventAttributeValue_CeString{
-					CeString: fmt.Sprintf("%f", locT.Latitude),
-				},
-			}
-			evt.Attributes[CeKeyLongitude] = &pb.CloudEventAttributeValue{
-				Attr: &pb.CloudEventAttributeValue_CeString{
-					CeString: fmt.Sprintf("%f", locT.Longitude),
-				},
-			}
-		default:
-			// TODO err
-		}
-	}
-	if preview := obj.Preview; preview != nil {
-		evt.Attributes[CeKeyPreview] = &pb.CloudEventAttributeValue{
-			Attr: &pb.CloudEventAttributeValue_CeUri{
-				CeUri: preview.GetLink().String(),
-			},
-		}
-	}
-	if summ := obj.Summary; summ != nil && len(summ) > 0 {
-		evt.Attributes[CeKeySummary] = &pb.CloudEventAttributeValue{
+	case inReplyTo.IsObject():
+		evt.Attributes[CeKeyInReplyTo] = &pb.CloudEventAttributeValue{
 			Attr: &pb.CloudEventAttributeValue_CeString{
-				CeString: summ.String(),
+				CeString: inReplyTo.(*vocab.Object).Name.String(),
 			},
 		}
+	default:
+		err = fmt.Errorf("%w \"inReplyTo\", unexpected type: %s", ErrFail, reflect.TypeOf(inReplyTo))
 	}
-	if tags := obj.Tag; tags != nil && len(tags) > 0 {
-		var cats []string
-		for _, tag := range tags {
-			switch tagT := tag.(type) {
-			case *vocab.Object:
-				cats = append(cats, tagT.Name.String())
-			default:
-				// TODO: err
-			}
-		}
-		evt.Attributes[CeKeyCategories] = &pb.CloudEventAttributeValue{
+	return
+}
+
+func convertLocation(loc vocab.Item, evt *pb.CloudEvent) (err error) {
+	switch locT := loc.(type) {
+	case *vocab.Place:
+		evt.Attributes[CeKeyLatitude] = &pb.CloudEventAttributeValue{
 			Attr: &pb.CloudEventAttributeValue_CeString{
-				CeString: strings.Join(cats, " "),
+				CeString: fmt.Sprintf("%f", locT.Latitude),
 			},
 		}
-	}
-	evt.Data = &pb.CloudEvent_TextData{
-		TextData: obj.Content.String(),
+		evt.Attributes[CeKeyLongitude] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeString{
+				CeString: fmt.Sprintf("%f", locT.Longitude),
+			},
+		}
+	default:
+		err = fmt.Errorf("%w location, unexpected type: %s", ErrFail, reflect.TypeOf(loc))
 	}
 	return
 }
--- service/converter/service_test.go
+++ service/converter/service_test.go
@@ -7,6 +7,7 @@ import (
 	vocab "github.com/go-ap/activitypub"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"testing"
 )
 
@@ -66,6 +67,58 @@ func TestService_Convert(t *testing.T) {
 	"published": "2024-02-16T15:07:30Z"
   }
 }`,
+			out: &pb.CloudEvent{
+				SpecVersion: "1.0",
+				Type:        "com.awakari.activitypub.v1",
+				Source:      "https://mastodon.social/users/johndoe",
+				Attributes: map[string]*pb.CloudEventAttributeValue{
+					"action": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "Create",
+						},
+					},
+					"attachment": {
+						Attr: &pb.CloudEventAttributeValue_CeUri{
+							CeUri: "https://files.mastodon.social/media_attachments/files/111/941/781/316/804/883/original/86df8fb1c70309c7.png",
+						},
+					},
+					"attachmenttype": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "image/png",
+						},
+					},
+					"cc": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "https://mastodon.social/users/akurilov/followers",
+						},
+					},
+					"object": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "Note",
+						},
+					},
+					"objecturl": {
+						Attr: &pb.CloudEventAttributeValue_CeUri{
+							CeUri: "https://mastodon.social/users/akurilov/statuses/111941782784824099",
+						},
+					},
+					"subject": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "John Doe",
+						},
+					},
+					"time": {
+						Attr: &pb.CloudEventAttributeValue_CeTimestamp{
+							CeTimestamp: &timestamppb.Timestamp{
+								Seconds: 1708096050,
+							},
+						},
+					},
+				},
+				Data: &pb.CloudEvent_TextData{
+					TextData: "<p>image test</p>",
+				},
+			},
 		},
 		"object instead of activity": {
 			actor: vocab.Actor{
@@ -84,6 +137,43 @@ func TestService_Convert(t *testing.T) {
   "to": "https://rhiaro.co.uk/followers/",
   "cc": "https://e14n.com/evan"
 }`,
+			out: &pb.CloudEvent{
+				SpecVersion: "1.0",
+				Type:        "com.awakari.activitypub.v1",
+				Source:      "https://mastodon.social/users/johndoe",
+				Attributes: map[string]*pb.CloudEventAttributeValue{
+					"subject": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "John Doe",
+						},
+					},
+					"cc": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "https://e14n.com/evan",
+						},
+					},
+					"object": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "Article",
+						},
+					},
+					"objecturl": {
+						Attr: &pb.CloudEventAttributeValue_CeUri{
+							CeUri: "https://rhiaro.co.uk/2016/05/minimal-activitypub",
+						},
+					},
+					"time": {
+						Attr: &pb.CloudEventAttributeValue_CeTimestamp{
+							CeTimestamp: &timestamppb.Timestamp{
+								Seconds: -62135596800,
+							},
+						},
+					},
+				},
+				Data: &pb.CloudEvent_TextData{
+					TextData: "[Today I finished morph, a client for posting ActivityStreams2...]\n\nToday I finished morph, a client for posting ActivityStreams2...",
+				},
+			},
 		},
 		"location": {
 			in: `
@@ -110,6 +200,165 @@ func TestService_Convert(t *testing.T) {
 	  }
   }
 }`,
+			out: &pb.CloudEvent{
+				SpecVersion: "1.0",
+				Type:        "com.awakari.activitypub.v1",
+				Attributes: map[string]*pb.CloudEventAttributeValue{
+					"action": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "Create",
+						},
+					},
+					"subject": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "[]",
+						},
+					},
+					"latitude": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "-73.974740",
+						},
+					},
+					"longitude": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "40.775630",
+						},
+					},
+					"object": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "Note",
+						},
+					},
+					"objecturl": {
+						Attr: &pb.CloudEventAttributeValue_CeUri{
+							CeUri: "https://location.edent.tel/9bc18f6eb339ec475c9bcfe69acf21fb",
+						},
+					},
+					"time": {
+						Attr: &pb.CloudEventAttributeValue_CeTimestamp{
+							CeTimestamp: &timestamppb.Timestamp{
+								Seconds: 1708096050,
+							},
+						},
+					},
+				},
+				Data: &pb.CloudEvent_TextData{
+					TextData: "I just checked-in to <a href=\"https://www.openstreetmap.org/way/958999496\">John Lennon's Imagine Mosaic</a>.",
+				},
+			},
+		},
+		"activitypub spec example 14": {
+			in: `
+{
+  "@context": ["https://www.w3.org/ns/activitystreams",
+               {"@language": "en"}],
+  "type": "Like",
+  "actor": "https://dustycloud.org/chris/",
+  "summary": "Chris liked 'Minimal ActivityPub update client'",
+  "object": "https://rhiaro.co.uk/2016/05/minimal-activitypub",
+  "to": ["https://rhiaro.co.uk/#amy",
+         "https://dustycloud.org/followers",
+         "https://rhiaro.co.uk/followers/"],
+  "cc": "https://e14n.com/evan"
+}`,
+			out: &pb.CloudEvent{
+				SpecVersion: "1.0",
+				Type:        "com.awakari.activitypub.v1",
+				Attributes: map[string]*pb.CloudEventAttributeValue{
+					"action": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "Like",
+						},
+					},
+					"subject": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "[]",
+						},
+					},
+					"objecturl": {
+						Attr: &pb.CloudEventAttributeValue_CeUri{
+							CeUri: "https://rhiaro.co.uk/2016/05/minimal-activitypub",
+						},
+					},
+					"time": {
+						Attr: &pb.CloudEventAttributeValue_CeTimestamp{
+							CeTimestamp: &timestamppb.Timestamp{
+								Seconds: -62135596800,
+							},
+						},
+					},
+				},
+				Data: &pb.CloudEvent_TextData{
+					TextData: "Chris liked 'Minimal ActivityPub update client'",
+				},
+			},
+		},
+		"activitystreams spec example 5": {
+			in: `
+{
+  "@context": "https://www.w3.org/ns/activitystreams",
+  "summary": "Martin added an article to his blog",
+  "type": "Add",
+  "published": "2015-02-10T15:04:55Z",
+  "actor": {
+   "type": "Person",
+   "id": "http://www.test.example/martin",
+   "name": "Martin Smith",
+   "url": "http://example.org/martin",
+   "image": {
+     "type": "Link",
+     "href": "http://example.org/martin/image.jpg",
+     "mediaType": "image/jpeg"
+   }
+  },
+  "object" : {
+   "id": "http://www.test.example/blog/abc123/xyz",
+   "type": "Article",
+   "url": "http://example.org/blog/2011/02/entry",
+   "name": "Why I love Activity Streams"
+  },
+  "target" : {
+   "id": "http://example.org/blog/",
+   "type": "OrderedCollection",
+   "name": "Martin's Blog"
+  }
+}`,
+			out: &pb.CloudEvent{
+				SpecVersion: "1.0",
+				Type:        "com.awakari.activitypub.v1",
+				Attributes: map[string]*pb.CloudEventAttributeValue{
+					"action": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "Add",
+						},
+					},
+					"subject": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "[]",
+						},
+					},
+					"object": {
+						Attr: &pb.CloudEventAttributeValue_CeString{
+							CeString: "Article",
+						},
+					},
+					"objecturl": {
+						Attr: &pb.CloudEventAttributeValue_CeUri{
+							CeUri: "http://www.test.example/blog/abc123/xyz",
+						},
+					},
+					"time": {
+						Attr: &pb.CloudEventAttributeValue_CeTimestamp{
+							CeTimestamp: &timestamppb.Timestamp{
+								Seconds: 1423580695,
+							},
+						},
+					},
+				},
+				Data: &pb.CloudEvent_TextData{
+					TextData: "Martin added an article to his blog",
+				},
+			},
 		},
 	}
 	for k, c := range cases {
@@ -118,7 +367,15 @@ func TestService_Convert(t *testing.T) {
 			err := json.Unmarshal([]byte(c.in), &activity)
 			require.Nil(t, err)
 			evt, err := svc.Convert(context.TODO(), c.actor, activity)
-			assert.Equal(t, c.out, evt)
+			if c.out == nil {
+				assert.Nil(t, evt)
+			} else {
+				assert.Equal(t, c.out.Type, evt.Type)
+				assert.Equal(t, c.out.Source, evt.Source)
+				assert.Equal(t, c.out.SpecVersion, evt.SpecVersion)
+				assert.Equal(t, c.out.Data, evt.Data)
+				assert.Equal(t, c.out.Attributes, evt.Attributes)
+			}
 			assert.ErrorIs(t, err, c.err)
 		})
 	}
