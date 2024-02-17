c612a3d9525529cc7fdd5aa919b50b518b539d96
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2024-02-17 10:24:19 +0200

    save [skip ci]

--- service/converter/service.go
+++ service/converter/service.go
@@ -1,15 +1,17 @@
 package converter
 
 import (
-    "context"
-    "github.com/cloudevents/sdk-go/binding/format/protobuf/v2/pb"
-    vocab "github.com/go-ap/activitypub"
-    "github.com/google/uuid"
-    "google.golang.org/protobuf/types/known/timestamppb"
+	"context"
+	"fmt"
+	"github.com/cloudevents/sdk-go/binding/format/protobuf/v2/pb"
+	vocab "github.com/go-ap/activitypub"
+	"github.com/google/uuid"
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"strings"
 )
 
 type Service interface {
-    Convert(ctx context.Context, actor vocab.Actor, activity vocab.Activity) (evt *pb.CloudEvent, err error)
+	Convert(ctx context.Context, actor vocab.Actor, activity vocab.Activity) (evt *pb.CloudEvent, err error)
 }
 
 type service struct {
@@ -18,87 +20,210 @@ type service struct {
 const CeType = "com.awakari.activitypub.v1"
 const CeSpecVersion = "1.0"
 const CeKeyAction = "action"
+const CeKeyAttachment = "attachment"
+const CeKeyAttachmentType = "attachmenttype"
+const CeKeyAudience = "audience"
 const CeKeyAuthor = "author"
+const CeKeyCategories = "categories"
+const CeKeyCc = "cc"
+const CeKeyImage = "image"
+const CeKeyInReplyTo = "inreplyto"
+const CeKeyLatitude = "latitude"
+const CeKeyLongitude = "longitude"
+const CeKeyPreview = "preview"
 const CeKeySubject = "subject"
+const CeKeySummary = "summary"
 const CeKeyTime = "time"
 
 const fmtAuthor = "<a href=\"%s\">%s</a>"
 
 func NewService() Service {
-    return service{}
+	return service{}
 }
 
 func (svc service) Convert(ctx context.Context, actor vocab.Actor, activity vocab.Activity) (evt *pb.CloudEvent, err error) {
-    // mastodon produces too much self delete activities, skip these
-    if activity.Type == vocab.DeleteType && activity.Actor == activity.Object {
-        return
-    }
-    //
-    evt = &pb.CloudEvent{
-        Id:          uuid.NewString(),
-        Source:      activity.Actor.GetLink().String(),
-        SpecVersion: CeSpecVersion,
-        Type:        CeType,
-        Attributes: map[string]*pb.CloudEventAttributeValue{
-            CeKeyAction: {
-                Attr: &pb.CloudEventAttributeValue_CeString{
-                    CeString: string(activity.Type),
-                },
-            },
-            CeKeyAuthor: {
-                Attr: &pb.CloudEventAttributeValue_CeString{
-                    CeString: actor.Name.String(),
-                },
-            },
-            CeKeyTime: {
-                Attr: &pb.CloudEventAttributeValue_CeTimestamp{
-                    CeTimestamp: timestamppb.New(activity.Published),
-                },
-            },
-        },
-    }
-    //
-    err = svc.convertObject(ctx, activity.Object.(*vocab.Object), evt)
-    //
-    return
+	// mastodon produces too much self delete activities, skip these
+	t := activity.Type
+	if t == vocab.DeleteType && activity.Actor == activity.Object {
+		return
+	}
+	//
+	evt, err = svc.convertActivity(ctx, actor, activity)
+	//
+	return
 }
 
-func (svc service) convertObject(ctx context.Context, o *vocab.Object, evt *pb.CloudEvent) (err error) {
-    evt.Attributes[CeKeySubject] = &pb.CloudEventAttributeValue{
-        Attr: &pb.CloudEventAttributeValue_CeString{
-            CeString: string(o.Type),
-        },
-    }
-    if att := o.Attachment; att != nil {
-        evt.Attributes[CeKeyAttachment] = &pb.CloudEventAttributeValue{
-            Attr: &pb.CloudEventAttributeValue_CeUri{
-                CeUri: att.GetLink().String(),
-            },
-        }
-    }
-    if summ := o.Summary; summ != nil && len(summ) > 0 {
-        evt.Attributes[CeKeySummary] = &pb.CloudEventAttributeValue{
-            Attr: &pb.CloudEventAttributeValue_CeString{
-                CeString: summ.String(),
-            },
-        }
-    }
-    if img := o.Image; img != nil {
-        evt.Attributes[CeKeyImage] = &pb.CloudEventAttributeValue{
-            Attr: &pb.CloudEventAttributeValue_CeUri{
-                CeUri: img.GetLink().String(),
-            },
-        }
-    }
-    if loc := o.Location; loc != nil {
-        evt.Attributes[CeKeyLatitude] = &pb.CloudEventAttributeValue{
-            Attr: &pb.CloudEventAttributeValue_CeString{
-               CeString: loc.,
-            },
-        }
-    }
-    evt.Data = &pb.CloudEvent_TextData{
-        TextData: o.Content.String(),
-    }
-    return
+func (svc service) convertActivity(ctx context.Context, actor vocab.Actor, activity vocab.Activity) (evt *pb.CloudEvent, err error) {
+	//
+	evt = &pb.CloudEvent{
+		Id:          uuid.NewString(),
+		Source:      actor.GetID().String(),
+		SpecVersion: CeSpecVersion,
+		Type:        CeType,
+		Attributes: map[string]*pb.CloudEventAttributeValue{
+			CeKeyAuthor: {
+				Attr: &pb.CloudEventAttributeValue_CeString{
+					CeString: actor.Name.String(),
+				},
+			},
+			CeKeyTime: {
+				Attr: &pb.CloudEventAttributeValue_CeTimestamp{
+					CeTimestamp: timestamppb.New(activity.Published),
+				},
+			},
+		},
+	}
+	//
+	t := string(activity.Type)
+	switch {
+	case activity.Object != nil && activity.Object.IsObject():
+		evt.Attributes[CeKeyAction] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeString{
+				CeString: t,
+			},
+		}
+		err = svc.convertObject(ctx, activity.Object.(*vocab.Object), evt)
+	default:
+		evt.Attributes[CeKeySubject] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeString{
+				CeString: t,
+			},
+		}
+		// TODO
+	}
+	//
+	return
+}
+
+func (svc service) convertObject(ctx context.Context, obj *vocab.Object, evt *pb.CloudEvent) (err error) {
+	evt.Attributes[CeKeySubject] = &pb.CloudEventAttributeValue{
+		Attr: &pb.CloudEventAttributeValue_CeString{
+			CeString: string(obj.Type),
+		},
+	}
+	if att := obj.Attachment; att != nil && att.IsObject() {
+		attObj := att.(*vocab.Object)
+		evt.Attributes[CeKeyAttachment] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeUri{
+				CeUri: attObj.URL.GetLink().String(),
+			},
+		}
+		evt.Attributes[CeKeyAttachmentType] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeString{
+				CeString: string(attObj.MediaType),
+			},
+		}
+	}
+	if aud := obj.Audience; aud != nil && len(aud) > 0 {
+		var auds []string
+		for _, audItem := range aud {
+			if audItem.IsObject() {
+				audName := audItem.(*vocab.Object).Name.String()
+				auds = append(auds, audName)
+			}
+		}
+		evt.Attributes[CeKeyAudience] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeString{
+				CeString: strings.Join(auds, " "),
+			},
+		}
+	}
+	if cc := obj.CC; cc != nil && len(cc) > 0 {
+		var ccs []string
+		for _, ccItem := range cc {
+			var ccStr string
+			switch {
+			case ccItem.IsLink():
+				ccStr = ccItem.GetLink().String()
+			case ccItem.IsObject():
+				ccStr = ccItem.(*vocab.Object).Name.String()
+			default:
+				// TODO err
+			}
+			if ccStr != "" {
+				ccs = append(ccs, ccStr)
+			}
+		}
+		evt.Attributes[CeKeyCc] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeString{
+				CeString: strings.Join(ccs, " "),
+			},
+		}
+	}
+	if img := obj.Image; img != nil {
+		evt.Attributes[CeKeyImage] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeUri{
+				CeUri: img.GetLink().String(),
+			},
+		}
+	}
+	if inReplyTo := obj.InReplyTo; inReplyTo != nil {
+		switch {
+		case inReplyTo.IsLink():
+			evt.Attributes[CeKeyImage] = &pb.CloudEventAttributeValue{
+				Attr: &pb.CloudEventAttributeValue_CeUri{
+					CeUri: inReplyTo.GetLink().String(),
+				},
+			}
+		case inReplyTo.IsObject():
+			evt.Attributes[CeKeyImage] = &pb.CloudEventAttributeValue{
+				Attr: &pb.CloudEventAttributeValue_CeString{
+					CeString: inReplyTo.(*vocab.Object).Name.String(),
+				},
+			}
+		default:
+			// TODO err
+		}
+	}
+	if loc := obj.Location; loc != nil {
+		switch locT := loc.(type) {
+		case *vocab.Place:
+			evt.Attributes[CeKeyLatitude] = &pb.CloudEventAttributeValue{
+				Attr: &pb.CloudEventAttributeValue_CeString{
+					CeString: fmt.Sprintf("%f", locT.Latitude),
+				},
+			}
+			evt.Attributes[CeKeyLongitude] = &pb.CloudEventAttributeValue{
+				Attr: &pb.CloudEventAttributeValue_CeString{
+					CeString: fmt.Sprintf("%f", locT.Longitude),
+				},
+			}
+		default:
+			// TODO err
+		}
+	}
+	if preview := obj.Preview; preview != nil {
+		evt.Attributes[CeKeyPreview] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeUri{
+				CeUri: preview.GetLink().String(),
+			},
+		}
+	}
+	if summ := obj.Summary; summ != nil && len(summ) > 0 {
+		evt.Attributes[CeKeySummary] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeString{
+				CeString: summ.String(),
+			},
+		}
+	}
+	if tags := obj.Tag; tags != nil && len(tags) > 0 {
+		var cats []string
+		for _, tag := range tags {
+			switch tagT := tag.(type) {
+			case *vocab.Object:
+				cats = append(cats, tagT.Name.String())
+			default:
+				// TODO: err
+			}
+		}
+		evt.Attributes[CeKeyCategories] = &pb.CloudEventAttributeValue{
+			Attr: &pb.CloudEventAttributeValue_CeString{
+				CeString: strings.Join(cats, " "),
+			},
+		}
+	}
+	evt.Data = &pb.CloudEvent_TextData{
+		TextData: obj.Content.String(),
+	}
+	return
 }
--- service/converter/service_test.go
+++ service/converter/service_test.go
@@ -18,52 +18,98 @@ func TestService_Convert(t *testing.T) {
 		out   *pb.CloudEvent
 		err   error
 	}{
-		"1": {
+		"mastodon post with image attached": {
 			actor: vocab.Actor{
+				ID:   "https://mastodon.social/users/johndoe",
 				Name: vocab.DefaultNaturalLanguageValue("John Doe"),
 			},
-			in: `{
-                  "id": "https://mastodon.social/users/akurilov/statuses/111941782784824099/activity",
-                  "type": "Create",
-                  "to": [
-                    "https://www.w3.org/ns/activitystreams#Public"
-                  ],
-                  "cc": [
-                    "https://mastodon.social/users/akurilov/followers"
-                  ],
-                  "published": "2024-02-16T15:07:30Z",
-                  "actor": "https://mastodon.social/users/akurilov",
-                  "object": {
-                    "id": "https://mastodon.social/users/akurilov/statuses/111941782784824099",
-                    "type": "Note",
-                    "content": "\u003cp\u003eimage test\u003c/p\u003e",
-                    "attachment": {
-                      "type": "Document",
-                      "mediaType": "image/png",
-                      "url": "https://files.mastodon.social/media_attachments/files/111/941/781/316/804/883/original/86df8fb1c70309c7.png"
-                    },
-                    "attributedTo": "https://mastodon.social/users/akurilov",
-                    "replies": {
-                      "id": "https://mastodon.social/users/akurilov/statuses/111941782784824099/replies",
-                      "type": "Collection",
-                      "first": {
-                        "type": "CollectionPage",
-                        "partOf": "https://mastodon.social/users/akurilov/statuses/111941782784824099/replies",
-                        "next": "https://mastodon.social/users/akurilov/statuses/111941782784824099/replies?only_other_accounts=true\u0026page=true",
-                        "totalItems": 0
-                      },
-                      "totalItems": 0
-                    },
-                    "url": "https://mastodon.social/@akurilov/111941782784824099",
-                    "to": [
-                      "https://www.w3.org/ns/activitystreams#Public"
-                    ],
-                    "cc": [
-                      "https://mastodon.social/users/akurilov/followers"
-                    ],
-                    "published": "2024-02-16T15:07:30Z"
-                  }
-                }`,
+			in: `
+{
+  "id": "https://mastodon.social/users/akurilov/statuses/111941782784824099/activity",
+  "type": "Create",
+  "to": [
+	"https://www.w3.org/ns/activitystreams#Public"
+  ],
+  "cc": [
+	"https://mastodon.social/users/akurilov/followers"
+  ],
+  "published": "2024-02-16T15:07:30Z",
+  "actor": "https://mastodon.social/users/akurilov",
+  "object": {
+	"id": "https://mastodon.social/users/akurilov/statuses/111941782784824099",
+	"type": "Note",
+	"content": "\u003cp\u003eimage test\u003c/p\u003e",
+	"attachment": {
+	  "type": "Document",
+	  "mediaType": "image/png",
+	  "url": "https://files.mastodon.social/media_attachments/files/111/941/781/316/804/883/original/86df8fb1c70309c7.png"
+	},
+	"attributedTo": "https://mastodon.social/users/akurilov",
+	"replies": {
+	  "id": "https://mastodon.social/users/akurilov/statuses/111941782784824099/replies",
+	  "type": "Collection",
+	  "first": {
+		"type": "CollectionPage",
+		"partOf": "https://mastodon.social/users/akurilov/statuses/111941782784824099/replies",
+		"next": "https://mastodon.social/users/akurilov/statuses/111941782784824099/replies?only_other_accounts=true\u0026page=true",
+		"totalItems": 0
+	  },
+	  "totalItems": 0
+	},
+	"url": "https://mastodon.social/@akurilov/111941782784824099",
+	"to": [
+	  "https://www.w3.org/ns/activitystreams#Public"
+	],
+	"cc": [
+	  "https://mastodon.social/users/akurilov/followers"
+	],
+	"published": "2024-02-16T15:07:30Z"
+  }
+}`,
+		},
+		"object instead of activity": {
+			actor: vocab.Actor{
+				ID:   "https://mastodon.social/users/johndoe",
+				Name: vocab.DefaultNaturalLanguageValue("John Doe"),
+			},
+			in: `
+{
+  "@context": ["https://www.w3.org/ns/activitystreams",
+               {"@language": "en-GB"}],
+  "id": "https://rhiaro.co.uk/2016/05/minimal-activitypub",
+  "type": "Article",
+  "name": "Minimal ActivityPub update client",
+  "content": "Today I finished morph, a client for posting ActivityStreams2...",
+  "attributedTo": "https://rhiaro.co.uk/#amy",
+  "to": "https://rhiaro.co.uk/followers/",
+  "cc": "https://e14n.com/evan"
+}`,
+		},
+		"location": {
+			in: `
+{
+  "id": "https://mastodon.social/users/akurilov/statuses/111941782784824099/activity",
+  "type": "Create",
+  "published": "2024-02-16T15:07:30Z",
+  "actor": "https://mastodon.social/users/akurilov",
+  "object": {
+	  "@context": "https://www.w3.org/ns/activitystreams",
+	  "id": "https://location.edent.tel/9bc18f6eb339ec475c9bcfe69acf21fb",
+	  "type": "Note",
+	  "published": "2024-01-28T12:13:38+00:00",
+	  "attributedTo": "https://location.edent.tel/edent_location",
+	  "content": "I just checked-in to <a href=\"https://www.openstreetmap.org/way/958999496\">John Lennon's Imagine Mosaic</a>.",
+	  "to": [
+		"https://www.w3.org/ns/activitystreams#Public"
+	  ],
+	  "location": {
+		"name": "John Lennon's Imagine",
+		"type": "Place",
+		"longitude": 40.77563,
+		"latitude": -73.97474
+	  }
+  }
+}`,
 		},
 	}
 	for k, c := range cases {
