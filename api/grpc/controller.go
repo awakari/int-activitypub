d6bce3999a1f28f214a93fc2290e1e57e5a6079b
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2024-06-08 22:31:19 +0300

    save [skip ci]

--- api/grpc/controller.go
+++ api/grpc/controller.go
@@ -28,7 +28,7 @@ func NewController(svc service.Service, search mastodon.Service) ServiceServer {
 
 func (c controller) Create(ctx context.Context, req *CreateRequest) (resp *CreateResponse, err error) {
 	resp = &CreateResponse{}
-	resp.Url, err = c.svc.RequestFollow(ctx, req.Addr, req.GroupId, req.UserId, "", "")
+	resp.Url, err = c.svc.RequestFollow(ctx, req.Addr, req.GroupId, req.UserId, req.SubId, req.Term)
 	err = encodeError(err)
 	return
 }
